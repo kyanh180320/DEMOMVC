d164cef89d90afcb07488742a3a106541881565f
kyanh180320 <54309137+kyanh180320@users.noreply.github.com>
2026-05-04 16:33:00 +0700

    init

---
+++ DEMO/Controllers/HelloWorldController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DEMO.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
